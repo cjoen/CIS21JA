27c904ea4d5431533a59dc2fb73f451506d701f4
cjoen <cj00wen@gmail.com>
2019-10-16 21:08:31 -0700

    creating 1st assignmen

--- assingments/assignment_3.asm
+++ assingments/assignment_3.asm
@@ -1,16 +1,24 @@
-TITLE assignment 3
+; Assignment 3 assembly calculations
 
 comment !
-Write a complete program that will input values for num1 ,num2, and num3 and display the
-value of the expression ( (num1 ^ 3) * num2 + 5 * ( num2 ^ 2) ) / num3.
 
-assume that the user enters only numbers that are greater than zero and the calculation never exceed 4 bytes size.
+name: Christopher Owen
+date: 10/16/19
+goal: take the input of 3 numbers and manipulate based on assinged caculations
+
 !
+
 
 include Irvine32.inc
 
 .data
 greeting BYTE "hello world", 0ah, 0dh, 0
+
+num1 dword 0
+num2 dword 0
+num3 dword 0
+
+; prompt to enter numbers
 
 .code
 main proc
@@ -20,6 +28,35 @@ main proc
 	mov	eax,5
 	add	eax,6
 
+  ; /////// this is where numbers are taken as input
+
+
+  ; //////
+
+
+
+  ; /////// This is where main calculations occur
+
+
+
+  ; ///////
+
+
+
+  ; /////// This is where the data is called to display
+
+
+  ; ///////
+
+
+
 	exit
 main endp
 end main
+
+
+comment !
+Result:
+
+
+!

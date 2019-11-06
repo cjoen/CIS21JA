71302ff0bc5aaa23d4cebb419ffd2ea8023de85b
Chris Owen - school comp <34991413+cjoen@users.noreply.github.com>
2019-11-06 11:23:10 -0800

    Update assignment_5.asm

--- assingments/assignment_5.asm
+++ assingments/assignment_5.asm
@@ -17,12 +17,15 @@ include Irvine32.inc
 
 ; # Format and text
 inputPrompt byte "enter a number: ", 0
-
-
-; # Array for numbers
+waitStr byte "Press any key to continue...", 0
 
 
 ; # Calculation variables
+var1 dword ?
+var2 dword ?
+
+sum dword ?
+difference dword ?
 
 
 
@@ -30,27 +33,21 @@ inputPrompt byte "enter a number: ", 0
 main proc
 ; /////////// Main code here
 
+; # A # Loop the prompt and run procs 3 times
 
-; # 1 # Loop the prompt and input method 10 times w/counter for Prompt
-
-mov edi, OFFSET arr
-mov ecx, LENGTHOF arr
-mov edx, OFFSET inputPrompt
+sub ecx,ecx
+mov ecx,3d
 
 L1:
-	call writeString
+	
+	; call locate display
 
-	call readDec
-	mov [edi], eax
-	add edi, type arr
+	call locate
+
+	call WaitForKey 
+
+
 loop L1
-
-
-; # 2 # Sum all variables and print
-
-
-
-; # 3 # ....
 
 
 
@@ -62,6 +59,80 @@ call crlf
 
 	exit
 main endp
+
+	; # 1 # Locate cursor on screen
+	locate PROC
+		
+		; locate
+
+		; call input
+
+		call input
+		ret
+	locate ENDP
+
+
+	; # 2 # Input the numbers
+	input PROC
+
+		mov edx, OFFSET inputPrompt
+		call writeString
+		call readInt
+		mov var1, eax
+
+		mov edx, OFFSET inputPrompt
+		call writeString
+		call readInt
+		mov var2, eax
+
+
+		;call sum and display
+		call DisplaySum
+
+		;call sub and display
+		call DisplayDiff
+		ret
+	input ENDP
+
+
+	; # 3 # Add and display the sum of values
+	DisplaySum PROC
+		;calculate sum of variables
+		mov eax, var1
+		add eax, var2
+
+		call writeInt
+		call crlf
+
+		mov sum, eax
+		ret
+	DisplaySum ENDP
+
+
+	; # 4 # Find difference and display value
+	DisplayDiff PROC
+		;calculate diff
+		mov eax, var1
+		sub eax, var2
+
+		call writeInt
+		call crlf
+
+		mov difference, eax
+		ret
+	DisplayDiff ENDP
+
+
+	; # 5 # Wait for key press
+	WaitForKey PROC
+		mov edx, OFFSET waitStr
+		call writeString
+		call readChar
+		call crlf
+		ret
+	WaitForKey ENDP
+
+
 end main
 
 ; //////////// Example run

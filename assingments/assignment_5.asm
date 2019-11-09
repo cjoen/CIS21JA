f78511911c518bd9d560d1382cb20125b781e410
cjoen <cj00wen@gmail.com>
2019-11-09 08:05:31 -0800

    assignment 5 completed

--- assingments/assignment_5.asm
+++ assingments/assignment_5.asm
@@ -6,9 +6,14 @@ date: 11/6/19
 
 Write a complete program that:
 
+		1. loops 3 times
+		 2. Clears screen
+		 3. prompts user for 2 signed numbers
+		 4. displays sum and difference
+		 5. uses irvine library
+		 6. uses procedures
 
 !
-
 
 include Irvine32.inc
 
@@ -16,7 +21,7 @@ include Irvine32.inc
 ; //////////// Data storage here
 
 ; # Format and text
-inputPrompt byte "enter a number: ", 0
+inputPrompt byte "enter any number: ", 0
 waitStr byte "Press any key to continue...", 0
 sumStr byte "sum: ", 0
 diffStr byte "difference: ", 0
@@ -41,72 +46,70 @@ sub ecx,ecx
 mov ecx,3d
 
 L1:
-	
+
 	; call locate display
-
 	call locate
-
-	call WaitForKey 
-
+	call WaitForKey
 
 loop L1
-
-
-
 ; # end of code #
 
 	exit
 main endp
 
-	; # 1 # Locate cursor on screen
-	locate PROC
-		
-		; locate
+
+; # 1 # Locate cursor on screen and call input process
+locate PROC
+
+		; Position cursor
 		mov   dh,15 ; row 15
 		mov   dl,40 ; column 40
 		call  Gotoxy; locate cursor
 
-
-		; call input
+		; call input process
 		call input
 
-		
-
 		ret
-	locate ENDP
+locate ENDP
 
 
-	; # 2 # Input the numbers
-	input PROC
+; # 2 # Prompt user and recieve input
+;				Also calls DisplaySum and DisplayDiff
+input PROC
 		sub eax,eax
 
+		; Store var 1
 		mov edx, OFFSET inputPrompt
 		call writeString
 		call readInt
 		mov var1, eax
 
+		; Position cursor
 		mov   dh,16 ; row 16
 		mov   dl,40 ; column 40
 		call  Gotoxy; locate cursor
 
+		; Store var 2
 		mov edx, OFFSET inputPrompt
 		call writeString
 		call readInt
 		mov var2, eax
-
 
 		;call sum and display
 		call DisplaySum
 
 		;call sub and display
 		call DisplayDiff
+
 		ret
 
-	input ENDP
+input ENDP
 
 
-	; # 3 # Add and display the sum of values
-	DisplaySum PROC
+; # 3 # Add and display the sum of values
+DisplaySum PROC
+
+		; Position cursor
 		mov   dh,17 ; row 17
 		mov   dl,40 ; column 40
 		call  Gotoxy; locate cursor
@@ -115,46 +118,55 @@ main endp
 		mov eax, var1
 		add eax, var2
 
+		; display sum of variables
 		mov edx, OFFSET sumStr
 		call writeString
 		call writeInt
 
 		mov sum, eax
+
 		ret
-	DisplaySum ENDP
+DisplaySum ENDP
 
 
-	; # 4 # Find difference and display value
-	DisplayDiff PROC
+; # 4 # Find difference and display value
+DisplayDiff PROC
+
+		; Position cursor
 		mov   dh,18 ; row 18
 		mov   dl,40 ; column 40
 		call  Gotoxy; locate cursor
 
-		;calculate diff
+		; calculate diff
 		mov eax, var1
 		sub eax, var2
 
+		; display difference
 		mov edx, OFFSET diffStr
 		call writeString
 		call writeInt
 
 		mov difference, eax
 		ret
-	DisplayDiff ENDP
+DisplayDiff ENDP
 
 
-	; # 5 # Wait for key press
-	WaitForKey PROC
+; # 5 # Wait for key press
+WaitForKey PROC
+
+		; Position cursor
 		mov   dh,19 ; row 19
 		mov   dl,40 ; column 40
 		call  Gotoxy; locate cursor
 
+		; prompt for keypress to continue
 		mov edx, OFFSET waitStr
 		call writeString
 		call readChar
 		call Clrscr
+
 		ret
-	WaitForKey ENDP
+WaitForKey ENDP
 
 
 end main
@@ -171,3 +183,23 @@ difference: +28
 Press any key to continue...
 
 !
+
+if ( w == 1 &&  x != 2)
+
+     y--;
+
+else
+
+    x++;
+
+
+cmp w,1
+JNE L1
+
+cmp w,2
+JE L2
+
+L1:
+
+
+L2:

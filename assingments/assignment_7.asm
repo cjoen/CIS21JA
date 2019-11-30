146673209700ca74d6c0c812fb742e8bbe0262cf
cjoen <cj00wen@gmail.com>
2019-11-29 22:29:17 -0800

    testing assignment 7

--- assingments/assignment_7.asm
+++ assingments/assignment_7.asm
@@ -1,31 +1,73 @@
-
-
-
 Title Assignment 7
 
 COMMENT !
+
+Assingment:
+  Ask user for up to 40 elements in an unsigned integer array
+  Display array before sort
+  Display array after sort
+
 *****************
-date:
+date: 11/29/2019
 *****************
 !
 
 include irvine32.inc
 ; ===============================================
 .data
-  
-  ; Fill your data here
+
+array dword 40 dup(?)
+arr_length dword ?
+inputPrompt BYTE "Enter upt to 40 unsigned integers:"
+
+inititalArr BYTE "These are the numbers stored in the array: "
+finalArr BYTE "These are the numbers after being sorted: "
+continut BYTE "Press any key to continue..."
 
 ;=================================================
 .code
 main proc
 
-  
-    
-	; FILL YOUR CODE HERE 
-	; YOU NEED TO CALL ENTER_ELEM, SORT_ARR AND PRINT_ARR PROCEDURES
-	; 
-	
-   
+  mov edx, offset inputPrompt
+  call writeString
+  call crlf
+
+  ; - call enter_elem -
+  push offset array
+  call enter_elem
+  pop arr_length
+
+  call crlf
+  call crlf
+
+  ; - call print_arr -
+  mov edx, offset inititalArr
+  call writeString
+  call crlf
+
+  push offset array
+  push arr_length
+  call print_arr
+  call crlf
+
+  ; - call sort_arr -
+  push offset array
+  push arr_length
+  call sort_arr
+
+  ; - call print_arr -
+  mov edx, offset finalArr
+  call writeString
+  call crlf
+  push offset array
+  push arr_length
+  call print_arr
+  call crlf
+
+  ; - finish -
+  mov edx, offset continue
+  call writeString
+  call readChar
 
    exit
 main endp
@@ -41,8 +83,40 @@ main endp
 ;   Fill the array and count the number of elements
 ;
 enter_elem proc
-	
-    ; FILL YOUR CODE HERE
+
+  push edp
+  mov edp, esp
+  push ebx
+  push ecx
+  push eax
+
+  mov ebx, [ebp+8]
+  xor ecx, ecx
+
+  comment!
+  code in c++:
+
+  !
+
+  InputL:
+    cmp ecx, 40
+    JGE endLoop
+    call readInt
+    cmp eax, 0
+    JE endLoop
+    mov [ebx], eax
+    add ebx, DWORD
+    inc ecx
+    jmp InputL
+
+  endLoop:
+
+  pop eax
+  mov [ebx+8], ecx
+  pop ecx
+  pop ebx
+  pop ebp
+  ret
 
 enter_elem endp
 
@@ -50,16 +124,50 @@ enter_elem endp
 ; void print_arr(arr_addr,arr_len)
 ;
 ; Input:
-;   ?
+;   Array address and length in the stack
 ; Output:
-;   ?
+;   N/A
 ; Operation:
 ;  print out the array
 ;
 
 print_arr proc
 
-  ; FILL YOUR CODE HERE
+  push ebp
+  mov ebp, esp
+  push edx
+  push ecx
+  push ebx
+  push eax
+
+  mov edx, [ebp + 12] ; the array offset
+  mov ecx, [ebp + 8]; the array length
+  xor ebx, ebx
+
+  comment!
+  conditional c++ code
+  !
+
+  printLoop:
+		cmp ebx, ecx
+		JGE outLoop
+
+		mov eax, [edx]
+		call writeDec
+		mov eax, ' '
+		call writeChar
+		add edx, DWORD
+		inc ebx
+		jmp printLoop
+	outLoop:
+
+	pop eax
+	pop ebx
+	pop ecx
+	pop edx
+	pop ebp
+
+	ret 8
 
 print_arr endp
 
@@ -67,17 +175,84 @@ print_arr endp
 ; void sort_arr(arr_addr,arr_len)
 ;
 ; Input:
-;   ?
+;   array address and length in stack
 ; Output:
-;   ?
+;   none
 ; Operation:
 ;   sort the array
 ;
 
 sort_arr proc
 
-   ; FILL YOUR CODE HERE
-   ; YOU NEED TO CALL COMPARE_AND_SWAP PROCEDURE 
+  push ebp
+  mov ebp, esp
+  push edx
+  push ecx
+  push ebx
+  push eax
+  push edi
+
+  mov ecx, [ebp + 8]; the array length
+  ; ecx = ecx * 4 ; to get the length in bytes not dword
+  mov eax, ecx
+  xor edx, edx
+  mov ecx, 4
+  mul ecx
+  mov ecx, eax
+
+  mov edx, [ebp + 12] ; the array offset
+  xor ebx, ebx  ; ebx will be used in inner loop
+  xor eax, eax  ; eax will be used in outer loop
+  xor edi, edi  ; esi will be used to save maxindex location
+
+  comment!
+  c++ code of proc
+  !
+
+  startSort:
+		cmp eax, ecx
+		jGE outSort
+
+		mov ebx, eax
+		mov edi, ebx
+
+		searchMaxIndex:
+			cmp ebx, ecx
+			jGE outSearch
+			push eax
+			mov eax, [edx + ebx]
+			cmp eax, [edx + edi]
+			pop eax
+			jLE notMax
+			mov edi, ebx
+			notMax:
+			add ebx, dword
+		jmp searchMaxIndex
+		outSearch:
+
+		add edx, eax
+		push edx
+		sub edx, eax
+
+		add edx, edi
+		push edx
+		sub edx, edi
+		call compare_and_swap
+
+		add eax, dword
+		jmp startSort
+	outSort:
+
+	pop edi
+	pop eax
+	pop ebx
+	pop ecx
+	pop edx
+	pop ebp
+
+	ret 8
+
+
 
 sort_arr endp
 
@@ -85,35 +260,88 @@ sort_arr endp
 ; void compare_and_swap(x_addr,y_addr)
 ;
 ; Input:
-;   ?
+;   x and y address in stack
 ; Output:
-;   ?
+;   none
 ; Operation:
-;  compare and call SWAP ONLY IF Y > X 
+;  compare and call SWAP ONLY IF Y > X
 ;
 
 compare_and_swap proc
 
-   ; FILL YOUR CODE HERE
-   ; YOU NEED TO CALL SWAP PROCEDURE 
+  push ebp
+  mov ebp, esp
+  push eax
+  push ebx
+  push ecx
+  push edx
+  mov eax, [ebp + 12] ; x address
+  mov ebx, [ebp + 8]  ; y address
 
+  mov ecx, [eax] ; ecx has x value
+  mov edx, [ebx] ; edx has y value
+  cmp ecx, edx
+  jGE dontSwap
+    push eax
+    push ebx
+    call swap
+  dontSwap:
+
+  pop edx
+  pop ecx
+  pop ebx
+  pop eax
+  pop ebp
+
+  ret 8
 compare_and_swap endp
 
 ; =================================================
 ; void swap(x_addr,y_addr)
 ;
 ; Input:
-;   ?
+;   x and y address in stack
 ; Output:
-;   ?
+;   none
 ; Operation:
 ;  swap the two inputs
 ;
 
 swap proc
 
-   ; FILL YOUR CODE HERE
+  push ebp
+  mov ebp, esp
+  push eax
+  push ebx
+  push ecx
+  push edx
+  mov eax, [ebp + 12] ; x address
+  mov ebx, [ebp + 8]  ; y address
+
+  mov ecx, [eax] ; ecx has x value
+  mov edx, [ebx] ; edx has y value
+  mov [eax], edx ; x address gets y value
+  mov [ebx], ecx ; y address gets x value
+
+  pop edx
+  pop ecx
+  pop ebx
+  pop eax
+  pop ebp
+
+  ret 8
 
 swap endp
 
 end main
+
+
+comment!
+
+output of code:
+---------------
+
+
+
+
+!
